252963150901baef8be2339772b29f949b056c8f
dai-yu <1435480344@qq.com>
2020-11-26 17:06:44 +0800

    [daiyu] create alice api

---
+++ alice/src/main/java/com/thoughtworks/capability/gtb/alice/HelloController.java
@@ -0,0 +1,16 @@
+package com.thoughtworks.capability.gtb.alice;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject("http://localhost:8081/hello", String.class);
+        return result;
+    }
+}
